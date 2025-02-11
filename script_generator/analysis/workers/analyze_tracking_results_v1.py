e2d228e1048e19926bd65e1a2f40a9abcc99dc7f
k00gar <arnaud.chevreux@gmail.com>
2025-02-11 23:05:15 +0100

    Fixed state.frame_end ghosting in the GUI, leading to early cut of processing of videos after a shorter one was already processed in the same GUI instance.

--- script_generator/analysis/workers/analyze_tracking_results_v1.py
+++ script_generator/analysis/workers/analyze_tracking_results_v1.py
@@ -35,7 +35,8 @@ def analyze_tracking_results_v1(state: AppState):
 
     # Deciding whether we start from there or from a user-specified later frame
     state.frame_start_track = max(max(first_penis_frame - int(state.video_info.fps), state.frame_start - int(state.video_info.fps)), 0)
-    state.frame_end = state.video_info.total_frames if not state.frame_end else state.frame_end
+    # state.frame_end = state.video_info.total_frames if not state.frame_end else state.frame_end
+    frame_end = state.video_info.total_frames if not state.frame_end else state.frame_end
     log_tr.info(f"Frame Start adjusted to first frame with a penis or penis tip: {state.frame_start_track}")
 
     video_info = state.video_info
@@ -87,7 +88,8 @@ def analyze_tracking_results_v1(state: AppState):
 
     for frame_pos in tqdm(
             # range(state.frame_start, state.frame_end), unit="f", desc="Analyzing tracking data", position=0,
-            range(state.frame_start, state.frame_end),
+            # range(state.frame_start, state.frame_end),
+            range(state.frame_start, frame_end),
             unit="f",
             desc="Analyzing tracking data", position=0,
             unit_scale=False,
@@ -237,13 +239,15 @@ def analyze_tracking_results_v1(state: AppState):
                 last_ui_update_time = current_time
                 elapsed_time = current_time - start_time
                 frames_processed = frame_pos - state.frame_start + 1
-                frames_remaining = state.frame_end - frame_pos - 1
+                # frames_remaining = state.frame_end - frame_pos - 1
+                frames_remaining = frame_end - frame_pos - 1
                 eta = (elapsed_time / frames_processed) * frames_remaining if frames_processed > 0 else 0
 
                 state.update_ui(ProgressMessage(
                     process="TRACKING_ANALYSIS",
                     frames_processed=frames_processed,
-                    total_frames=state.frame_end,
+                    # total_frames=state.frame_end,
+                    total_frames=frame_end,
                     eta=time.strftime("%H:%M:%S", time.gmtime(eta)) if eta != float('inf') else "Calculating..."
                 ))
 
