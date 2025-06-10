873232c3a8f0091b473e8953ddbd50ce7eaa8dba
k00gar <arnaud.chevreux@gmail.com>
2025-06-10 18:34:45 +0200

    Fixed multiprocessing for Linux and CUDA

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import multiprocessing
+import platform
 from application.gui_components.app_gui import GUI
 from application.logic.app_logic import ApplicationLogic
 
 if __name__ == "__main__":
+    if platform.system() != "Windows":
+        multiprocessing.set_start_method('spawn', force=True)
+
     core_app = ApplicationLogic()
     gui = GUI(app_logic=core_app)
     core_app.gui_instance = gui
